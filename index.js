4890b82390e41f925f2bcbf814fb7a475518caf3
Kenny Clare <kmitchellclare@gmail.com>
2022-05-02 07:35:09 -0700

    commit tree d3daa27ad0a2989a32fb4b167f6bc148070bc633

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+]
+
+// function superbowlWin(year, result) {
+//     let win = null
+//     for(let item of year) {
+//         if(result(item) === '2015') {
+//             win = item
+//             break;
+//         }
+//     }
+//     return win
+// }
+
+function superbowlWin(record) {
+    const winningYear = record.find(winResult => winResult.result === "W")
+    if(winningYear) {
+        return winningYear.year
+    }
+}
+
+
+
+// function superbowlWin(record) {
+//     const win = record.find(w => {
+//         if(w.result === "W") {
+//             return w
+//         }
+//     })
+//     if(win) {
+//         return win.year
+//     } else {
+//         return win
+//     }
+//
